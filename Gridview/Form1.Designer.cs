6e60c5a751262c9d848aa12fd26056ebf105de39
Lab2-15 <Lab2-15@LAB2-15>
2025-04-01 18:53:50 -0600

    falta actualizar agregar y limpiar tienes el ejemplo del profe

--- Gridview/Form1.Designer.cs
+++ Gridview/Form1.Designer.cs
@@ -38,6 +38,10 @@
             this.label2 = new System.Windows.Forms.Label();
             this.txbNombre = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txbCodeagregar = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txbEnfermedad = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -68,6 +72,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
             this.label1.Location = new System.Drawing.Point(56, 445);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(32, 13);
@@ -76,6 +81,7 @@
             // 
             // txbcode
             // 
+            this.txbcode.BackColor = System.Drawing.Color.MistyRose;
             this.txbcode.Location = new System.Drawing.Point(24, 475);
             this.txbcode.Name = "txbcode";
             this.txbcode.Size = new System.Drawing.Size(95, 20);
@@ -94,6 +100,7 @@
             // 
             // btnLimpiar
             // 
+            this.btnLimpiar.ForeColor = System.Drawing.SystemColors.Highlight;
             this.btnLimpiar.Location = new System.Drawing.Point(440, 11);
             this.btnLimpiar.Margin = new System.Windows.Forms.Padding(2);
             this.btnLimpiar.Name = "btnLimpiar";
@@ -105,6 +112,7 @@
             // 
             // txbAnimal
             // 
+            this.txbAnimal.BackColor = System.Drawing.Color.MistyRose;
             this.txbAnimal.Location = new System.Drawing.Point(509, 387);
             this.txbAnimal.Name = "txbAnimal";
             this.txbAnimal.Size = new System.Drawing.Size(78, 20);
@@ -113,6 +121,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
             this.label2.Location = new System.Drawing.Point(515, 367);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(38, 13);
@@ -121,6 +130,7 @@
             // 
             // txbNombre
             // 
+            this.txbNombre.BackColor = System.Drawing.Color.MistyRose;
             this.txbNombre.Location = new System.Drawing.Point(509, 429);
             this.txbNombre.Name = "txbNombre";
             this.txbNombre.Size = new System.Drawing.Size(78, 20);
@@ -129,11 +139,48 @@
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.Highlight;
             this.label3.Location = new System.Drawing.Point(515, 413);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(44, 13);
             this.label3.TabIndex = 9;
             this.label3.Text = "Nombre";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label4.Location = new System.Drawing.Point(515, 461);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Code";
+            // 
+            // txbCodeagregar
+            // 
+            this.txbCodeagregar.BackColor = System.Drawing.Color.MistyRose;
+            this.txbCodeagregar.Location = new System.Drawing.Point(509, 477);
+            this.txbCodeagregar.Name = "txbCodeagregar";
+            this.txbCodeagregar.Size = new System.Drawing.Size(78, 20);
+            this.txbCodeagregar.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label5.Location = new System.Drawing.Point(515, 511);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Enfermedad";
+            // 
+            // txbEnfermedad
+            // 
+            this.txbEnfermedad.BackColor = System.Drawing.Color.MistyRose;
+            this.txbEnfermedad.Location = new System.Drawing.Point(509, 527);
+            this.txbEnfermedad.Name = "txbEnfermedad";
+            this.txbEnfermedad.Size = new System.Drawing.Size(78, 20);
+            this.txbEnfermedad.TabIndex = 13;
             // 
             // Form1
             // 
@@ -141,6 +188,10 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.PapayaWhip;
             this.ClientSize = new System.Drawing.Size(615, 572);
+            this.Controls.Add(this.txbEnfermedad);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txbCodeagregar);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.txbNombre);
             this.Controls.Add(this.label2);
@@ -172,6 +223,10 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txbNombre;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txbCodeagregar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txbEnfermedad;
     }
 }
 
